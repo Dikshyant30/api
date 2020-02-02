1de1b1b0c1565619f452c0302066d2669cdfd7ac
Dikshyant <dikshyant1997@gmail.com>
2020-02-02 08:51:01 +0530

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock==0 ? 'Out Of Stock ':stock,
+            'discount'=>$this->discount,
+            //'totatPrice'=> round((1 - ($this->discount/100)) *  $this->discount,2),
+            'ratings'=> $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2)
+            : 'No Ratings Yet',
+            'href'=>[
+               'reviews'=>route('reviews.index',$this->id)
+            ]
         ];
     }
 }
